bd8b978ede2e51eb9cf34b7ebadf777c1989b7fa
reginaleto <80128727+reginaleto@users.noreply.github.com>
2022-06-14 14:21:01 -0400

    Add files via upload

---
+++ Lab4_Regina_Leto/frmCraps.Designer.cs
@@ -0,0 +1,335 @@
+﻿
+namespace Lab4_Regina_Leto
+{
+    partial class frmCraps
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRoll = new System.Windows.Forms.Button();
+            this.lblFirstDice = new System.Windows.Forms.Label();
+            this.lblSecondDie = new System.Windows.Forms.Label();
+            this.lblSecondDieNum = new System.Windows.Forms.Label();
+            this.lblFirstDieNum = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.lblTotalNum = new System.Windows.Forms.Label();
+            this.picDie2 = new System.Windows.Forms.PictureBox();
+            this.picDie1 = new System.Windows.Forms.PictureBox();
+            this.lblPointNum = new System.Windows.Forms.Label();
+            this.lblPoints = new System.Windows.Forms.Label();
+            this.lblScoreOutput = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.picPuck2 = new System.Windows.Forms.PictureBox();
+            this.picPuck3 = new System.Windows.Forms.PictureBox();
+            this.picPuck4 = new System.Windows.Forms.PictureBox();
+            this.picPuck5 = new System.Windows.Forms.PictureBox();
+            this.picPuck6 = new System.Windows.Forms.PictureBox();
+            this.picPuck1 = new System.Windows.Forms.PictureBox();
+            this.picOffPuck = new System.Windows.Forms.PictureBox();
+            this.lblComments = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picDie2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDie1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPuck2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPuck3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPuck4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPuck5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPuck6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPuck1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picOffPuck)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnRoll
+            // 
+            this.btnRoll.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRoll.Location = new System.Drawing.Point(24, 704);
+            this.btnRoll.Name = "btnRoll";
+            this.btnRoll.Size = new System.Drawing.Size(162, 76);
+            this.btnRoll.TabIndex = 0;
+            this.btnRoll.Text = "Roll";
+            this.btnRoll.UseVisualStyleBackColor = true;
+            this.btnRoll.Click += new System.EventHandler(this.btnRoll_Click);
+            // 
+            // lblFirstDice
+            // 
+            this.lblFirstDice.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFirstDice.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblFirstDice.Location = new System.Drawing.Point(1340, 138);
+            this.lblFirstDice.Name = "lblFirstDice";
+            this.lblFirstDice.Size = new System.Drawing.Size(50, 23);
+            this.lblFirstDice.TabIndex = 3;
+            this.lblFirstDice.Text = "Die 1";
+            this.lblFirstDice.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // lblSecondDie
+            // 
+            this.lblSecondDie.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSecondDie.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblSecondDie.Location = new System.Drawing.Point(1343, 171);
+            this.lblSecondDie.Name = "lblSecondDie";
+            this.lblSecondDie.Size = new System.Drawing.Size(50, 23);
+            this.lblSecondDie.TabIndex = 4;
+            this.lblSecondDie.Text = "Die 2";
+            this.lblSecondDie.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // lblSecondDieNum
+            // 
+            this.lblSecondDieNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSecondDieNum.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblSecondDieNum.Location = new System.Drawing.Point(1395, 171);
+            this.lblSecondDieNum.Name = "lblSecondDieNum";
+            this.lblSecondDieNum.Size = new System.Drawing.Size(50, 23);
+            this.lblSecondDieNum.TabIndex = 5;
+            // 
+            // lblFirstDieNum
+            // 
+            this.lblFirstDieNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFirstDieNum.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblFirstDieNum.Location = new System.Drawing.Point(1395, 138);
+            this.lblFirstDieNum.Name = "lblFirstDieNum";
+            this.lblFirstDieNum.Size = new System.Drawing.Size(50, 23);
+            this.lblFirstDieNum.TabIndex = 6;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblTotal.Location = new System.Drawing.Point(1343, 225);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(50, 23);
+            this.lblTotal.TabIndex = 8;
+            this.lblTotal.Text = "Total";
+            this.lblTotal.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // lblTotalNum
+            // 
+            this.lblTotalNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalNum.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblTotalNum.Location = new System.Drawing.Point(1395, 225);
+            this.lblTotalNum.Name = "lblTotalNum";
+            this.lblTotalNum.Size = new System.Drawing.Size(50, 23);
+            this.lblTotalNum.TabIndex = 7;
+            // 
+            // picDie2
+            // 
+            this.picDie2.Location = new System.Drawing.Point(293, 716);
+            this.picDie2.Name = "picDie2";
+            this.picDie2.Size = new System.Drawing.Size(50, 50);
+            this.picDie2.TabIndex = 10;
+            this.picDie2.TabStop = false;
+            // 
+            // picDie1
+            // 
+            this.picDie1.Location = new System.Drawing.Point(227, 716);
+            this.picDie1.Name = "picDie1";
+            this.picDie1.Size = new System.Drawing.Size(51, 50);
+            this.picDie1.TabIndex = 9;
+            this.picDie1.TabStop = false;
+            // 
+            // lblPointNum
+            // 
+            this.lblPointNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPointNum.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblPointNum.Location = new System.Drawing.Point(1395, 331);
+            this.lblPointNum.Name = "lblPointNum";
+            this.lblPointNum.Size = new System.Drawing.Size(50, 23);
+            this.lblPointNum.TabIndex = 13;
+            // 
+            // lblPoints
+            // 
+            this.lblPoints.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPoints.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblPoints.Location = new System.Drawing.Point(1340, 331);
+            this.lblPoints.Name = "lblPoints";
+            this.lblPoints.Size = new System.Drawing.Size(50, 23);
+            this.lblPoints.TabIndex = 12;
+            this.lblPoints.Text = "Points";
+            this.lblPoints.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // lblScoreOutput
+            // 
+            this.lblScoreOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScoreOutput.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblScoreOutput.Location = new System.Drawing.Point(1395, 401);
+            this.lblScoreOutput.Name = "lblScoreOutput";
+            this.lblScoreOutput.Size = new System.Drawing.Size(50, 23);
+            this.lblScoreOutput.TabIndex = 15;
+            // 
+            // lblScore
+            // 
+            this.lblScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblScore.Location = new System.Drawing.Point(1340, 401);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(50, 23);
+            this.lblScore.TabIndex = 14;
+            this.lblScore.Text = "Score";
+            this.lblScore.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // picPuck2
+            // 
+            this.picPuck2.Image = global::Lab4_Regina_Leto.Properties.Resources.puck;
+            this.picPuck2.Location = new System.Drawing.Point(478, 124);
+            this.picPuck2.Name = "picPuck2";
+            this.picPuck2.Size = new System.Drawing.Size(24, 25);
+            this.picPuck2.TabIndex = 16;
+            this.picPuck2.TabStop = false;
+            // 
+            // picPuck3
+            // 
+            this.picPuck3.Image = global::Lab4_Regina_Leto.Properties.Resources.puck;
+            this.picPuck3.Location = new System.Drawing.Point(586, 124);
+            this.picPuck3.Name = "picPuck3";
+            this.picPuck3.Size = new System.Drawing.Size(24, 25);
+            this.picPuck3.TabIndex = 17;
+            this.picPuck3.TabStop = false;
+            // 
+            // picPuck4
+            // 
+            this.picPuck4.Image = global::Lab4_Regina_Leto.Properties.Resources.puck;
+            this.picPuck4.Location = new System.Drawing.Point(669, 124);
+            this.picPuck4.Name = "picPuck4";
+            this.picPuck4.Size = new System.Drawing.Size(24, 25);
+            this.picPuck4.TabIndex = 18;
+            this.picPuck4.TabStop = false;
+            // 
+            // picPuck5
+            // 
+            this.picPuck5.Image = global::Lab4_Regina_Leto.Properties.Resources.puck;
+            this.picPuck5.Location = new System.Drawing.Point(770, 124);
+            this.picPuck5.Name = "picPuck5";
+            this.picPuck5.Size = new System.Drawing.Size(24, 25);
+            this.picPuck5.TabIndex = 19;
+            this.picPuck5.TabStop = false;
+            // 
+            // picPuck6
+            // 
+            this.picPuck6.Image = global::Lab4_Regina_Leto.Properties.Resources.puck;
+            this.picPuck6.Location = new System.Drawing.Point(869, 124);
+            this.picPuck6.Name = "picPuck6";
+            this.picPuck6.Size = new System.Drawing.Size(24, 25);
+            this.picPuck6.TabIndex = 20;
+            this.picPuck6.TabStop = false;
+            // 
+            // picPuck1
+            // 
+            this.picPuck1.Image = global::Lab4_Regina_Leto.Properties.Resources.puck;
+            this.picPuck1.Location = new System.Drawing.Point(386, 124);
+            this.picPuck1.Name = "picPuck1";
+            this.picPuck1.Size = new System.Drawing.Size(24, 25);
+            this.picPuck1.TabIndex = 21;
+            this.picPuck1.TabStop = false;
+            // 
+            // picOffPuck
+            // 
+            this.picOffPuck.Image = global::Lab4_Regina_Leto.Properties.Resources.puck2;
+            this.picOffPuck.Location = new System.Drawing.Point(994, 84);
+            this.picOffPuck.Name = "picOffPuck";
+            this.picOffPuck.Size = new System.Drawing.Size(26, 26);
+            this.picOffPuck.TabIndex = 22;
+            this.picOffPuck.TabStop = false;
+            // 
+            // lblComments
+            // 
+            this.lblComments.BackColor = System.Drawing.Color.Transparent;
+            this.lblComments.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblComments.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblComments.ForeColor = System.Drawing.Color.White;
+            this.lblComments.Location = new System.Drawing.Point(1086, 640);
+            this.lblComments.Name = "lblComments";
+            this.lblComments.Size = new System.Drawing.Size(359, 126);
+            this.lblComments.TabIndex = 23;
+            // 
+            // frmCraps
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImage = global::Lab4_Regina_Leto.Properties.Resources.table;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(1496, 792);
+            this.Controls.Add(this.lblComments);
+            this.Controls.Add(this.picOffPuck);
+            this.Controls.Add(this.picPuck1);
+            this.Controls.Add(this.picPuck6);
+            this.Controls.Add(this.picPuck5);
+            this.Controls.Add(this.picPuck4);
+            this.Controls.Add(this.picPuck3);
+            this.Controls.Add(this.picPuck2);
+            this.Controls.Add(this.lblScoreOutput);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.lblPointNum);
+            this.Controls.Add(this.lblPoints);
+            this.Controls.Add(this.picDie2);
+            this.Controls.Add(this.picDie1);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblTotalNum);
+            this.Controls.Add(this.lblFirstDieNum);
+            this.Controls.Add(this.lblSecondDieNum);
+            this.Controls.Add(this.lblSecondDie);
+            this.Controls.Add(this.lblFirstDice);
+            this.Controls.Add(this.btnRoll);
+            this.Name = "frmCraps";
+            this.Text = "Craps!";
+            this.Load += new System.EventHandler(this.frmCraps_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picDie2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDie1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPuck2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPuck3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPuck4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPuck5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPuck6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPuck1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picOffPuck)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRoll;
+        private System.Windows.Forms.Label lblFirstDice;
+        private System.Windows.Forms.Label lblSecondDie;
+        private System.Windows.Forms.Label lblSecondDieNum;
+        private System.Windows.Forms.Label lblFirstDieNum;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label lblTotalNum;
+        private System.Windows.Forms.PictureBox picDie1;
+        private System.Windows.Forms.PictureBox picDie2;
+        private System.Windows.Forms.Label lblPointNum;
+        private System.Windows.Forms.Label lblPoints;
+        private System.Windows.Forms.Label lblScoreOutput;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.PictureBox picPuck2;
+        private System.Windows.Forms.PictureBox picPuck3;
+        private System.Windows.Forms.PictureBox picPuck4;
+        private System.Windows.Forms.PictureBox picPuck5;
+        private System.Windows.Forms.PictureBox picPuck6;
+        private System.Windows.Forms.PictureBox picPuck1;
+        private System.Windows.Forms.PictureBox picOffPuck;
+        private System.Windows.Forms.Label lblComments;
+    }
+}
+
---
+++ Lab4_Regina_Leto/frmCraps.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Lab4_Regina_Leto
+{
+    public partial class frmCraps : Form
+    {
+        /* ‘Lab 4
+‘EGR 1400 - Winter 2021
+‘Regina Leto
+‘I fully understand the following statement.
+‘OU PLAGIARISM POLICY
+‘All members of the academic community at Oakland are expected to practice and uphold ‘standards
+of academic integrity and honesty. An instructor is expected to inform and instruct ‘students about
+the procedures and standards of research and documentation required of students ‘in fulfilling
+course work. A student is expected to follow such instructions and be sure the rules ‘and procedures
+are understood in order to avoid inadvertent misrepresentation of his work. ‘Students must assume
+that individual (unaided) work on exams and lab reports and documentation ‘of sources is expected
+unless the instructor specifically says that is not necessary.
+‘The following definitions are some examples of academic dishonesty:
+ ‘Plagiarizing from work of others. Plagiarism is using someone else's work or ideas without
+‘giving the other person credit; by doing this, a student is, in effect, claiming credit for
+‘someone else's thinking. Whether the student has read or heard the information he/she uses,
+‘the student must document the source of information. When dealing with written sources,
+‘a clear distinction would be made between quotations (which reproduce information from
+‘the source word-for-word within quotation marks) and paraphrases (which digest the
+‘source information and produce it in the student's own words). Both direct quotations and
+‘paraphrases must be documented. Just because a student rephrases, condenses or selects
+‘from another person's work, the ideas are still the other person's, and failure to give ‘credit
+constitutes misrepresentation of the student's actual work and plagiarism of ‘another's ideas.
+Naturally, buying a paper and handing it in as one's own work is ‘plagiarism.
+ ‘Cheating on lab reports falsifying data or submitting data not based on student's own work.
+*/
+
+        //random nums and total will show up on the side of the form
+        Random randomDice = new Random();
+
+        //form level variables
+        int firstDiceRoll;
+        int secondDiceRoll;
+        int total;
+        int points;
+        int score = 0;
+        int state = 1;
+        public void DicePic()
+        {
+            switch (firstDiceRoll) // will change first dice picture to match first random number
+            {
+                case 1:
+                    picDie1.BackgroundImage = Properties.Resources.die1;
+                    break;
+                case 2:
+                    picDie1.BackgroundImage = Properties.Resources.die2;
+                    break;
+                case 3:
+                    picDie1.BackgroundImage = Properties.Resources.die3;
+                    break;
+                case 4:
+                    picDie1.BackgroundImage = Properties.Resources.die4;
+                    break;
+                case 5:
+                    picDie1.BackgroundImage = Properties.Resources.die5;
+                    break;
+                case 6:
+                    picDie1.BackgroundImage = Properties.Resources.die6;
+                    break;
+            }
+            switch (secondDiceRoll) // will change second dice picture to match second random number
+            {
+                case 1:
+                    picDie2.BackgroundImage = Properties.Resources.die1;
+                    break;
+                case 2:
+                    picDie2.BackgroundImage = Properties.Resources.die2;
+                    break;
+                case 3:
+                    picDie2.BackgroundImage = Properties.Resources.die3;
+                    break;
+                case 4:
+                    picDie2.BackgroundImage = Properties.Resources.die4;
+                    break;
+                case 5:
+                    picDie2.BackgroundImage = Properties.Resources.die5;
+                    break;
+                case 6:
+                    picDie2.BackgroundImage = Properties.Resources.die6;
+                    break;
+            }
+        }
+        public void PuckPic() 
+        {
+            // default settings
+            picOffPuck.Visible = true;
+            picPuck1.Visible = false;
+            picPuck2.Visible = false;
+            picPuck3.Visible = false;
+            picPuck4.Visible = false;
+            picPuck5.Visible = false;
+            picPuck6.Visible = false;
+
+            switch (points) // switches through the puck pics and changes based on what number is rolled
+            {
+                case 2:
+                case 3:
+                case 7:
+                case 11:
+                case 12:
+                    picOffPuck.Visible = true;
+                    break;
+                case 4:
+                    picPuck1.Visible = true;
+                    break;
+                case 5:
+                    picPuck2.Visible = true;
+                    break;
+                case 6:
+                    picPuck3.Visible = true;
+                    break;
+                case 8:
+                    picPuck4.Visible = true;
+                    break;
+                case 9:
+                    picPuck5.Visible = true;
+                    break;
+                case 10:
+                    picPuck6.Visible = true;
+                    break;
+            }
+           
+        }
+        public void DiceRoll()
+        { // game play method
+
+            if (state == 1) // auto wins and losses will be addressed as such, if any other number rolled, state changes to 2
+            {
+                if (total == 7 || total == 11) // || = or; works instead of commas
+                {
+                    lblComments.Text = "Congratulations, automatic win! Click 'Roll' to play again.";
+                    score++;
+                    lblScoreOutput.Text = score.ToString();
+
+                    Reset();
+                }
+                else if (total == 2 || total == 3 || total == 12)
+                {
+                    lblComments.Text = "Sorry, automatic loss. Click 'Roll' to play again!";
+                    score--;
+                    lblScoreOutput.Text = score.ToString();
+
+                    Reset();
+                }
+                else // make SecondRoll method and make sure the puck pics stay with corresponding first rolls
+                {
+                    lblComments.Text = "Keep rolling! Click 'Roll' to roll again";
+                    state = 2;
+                    points = total;
+                    lblPointNum.Text = points.ToString();
+                }
+            }
+            else // anything other than 2, 3, 7, 11, 12
+            {
+                if (points == total) // if total from second roll equals points from first roll, you win
+                {
+                    lblComments.Text = "Congratulations, you win! Click 'Roll' to play again.";
+                    state = 1;
+                    score++;
+
+                    Reset();
+                }
+                else if (total == 7) // if second roll total = 7, you lose
+                {
+                    lblComments.Text = "Sorry, you lose. Click 'Roll' to play again!";
+                    state = 1;
+                    score--;
+                    lblScoreOutput.Text = score.ToString();
+
+                    Reset();
+                }
+                else // will keep going until second roll total = first roll points or 7
+                {
+                    lblComments.Text = " Keep rolling!";
+                    state = 2;
+                }
+            }
+        }
+        public void Reset()
+        {
+            lblFirstDieNum.Text = "";
+            lblSecondDieNum.Text = "";
+            lblPointNum.Text = "  ";
+            picPuck1.Visible = false;
+            picPuck2.Visible = false;
+            picPuck3.Visible = false;
+            picPuck4.Visible = false;
+            picPuck5.Visible = false;
+            picPuck6.Visible = false;
+            picOffPuck.Visible = true;
+            picDie1.Visible = false;
+            picDie2.Visible = false; 
+        }
+        public frmCraps()
+        {
+            InitializeComponent();
+        }
+        private void btnRoll_Click(object sender, EventArgs e)
+        {
+            // generates random numbers for the first and second dice and will assign them to the variables associated
+            firstDiceRoll = randomDice.Next(1, 7);
+            secondDiceRoll = randomDice.Next(1, 7);
+            total = firstDiceRoll + secondDiceRoll; // total number on both dice
+            lblFirstDieNum.Text = firstDiceRoll.ToString();
+            lblSecondDieNum.Text = secondDiceRoll.ToString();
+            lblTotalNum.Text = total.ToString();
+            
+            DicePic();
+            DiceRoll();
+            PuckPic();
+
+            lblScoreOutput.Text = score.ToString();
+        }
+        private void frmCraps_Load(object sender, EventArgs e)
+        {
+            picPuck1.Visible = false;
+            picPuck2.Visible = false;
+            picPuck3.Visible = false;
+            picPuck4.Visible = false;
+            picPuck5.Visible = false;
+            picPuck6.Visible = false; 
+        }
+
+    }
+}
